50180a686bb036871fbd95e0eeb552ce67f1db53
Furina <poonammishra0663234@gmail.com>
2026-01-20 06:08:33 +0530

    Delete netlify/functions directory

--- netlify/functions/login.js
+++
@@ -1,8 +0,0 @@
-export async function handler() {
-  return {
-    statusCode: 302,
-    headers: {
-      Location: "https://discord.com/oauth2/authorize?client_id=1462876315915915264&scope=identify%20guilds&response_type=code"
-    }
-  };
-}
